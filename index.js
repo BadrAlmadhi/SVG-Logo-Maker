d143340ede8db34d6786c7e277caeea706f00bd5
Badr Almadhi <badr.almadhi95@gmail.com>
2024-01-16 17:48:08 -0700

    shapes created but color do not apply will fix the problem later

--- index.js
+++ index.js
@@ -5,47 +5,86 @@
 // select shape circle, square, triangle
 // shape color
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lip/shapes");
 
+class Svg {
+  constructor() {
+    this.name = "";
+    this.text = "";
+    this.shape = null;
+  }
 
-const inquire = require("inquirer");
-const fs = require('node:fs'); //write to shapes.svg
-const shapes = require("./lip/shapes");
+  render() {
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.text}${this.shape}</svg>`;
+  }
 
+  setText(text, color) {
+    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
 
+  setShape(shape) {
+    this.shape = shape.render();
+  }
 
-class File {
-    constructor(){
-        this.name = '';
-        this.shape = '';
+  async promptUser() {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message:
+          "Enter your logo name (Please note the logo name should be up to 3 characters):",
+        validate: function (input) {
+          return input.length <= 3 ? true : "Logo name should be up to 3 characters.";
+        },
+      },
+      {
+        type: "input",
+        name: "colorShape",
+        message: "Enter the color of your shape:",
+      },
+      {
+        type: "input",
+        name: "colorText",
+        message: "Enter the color of your text:",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Select your shape style:",
+        choices: ["Square", "Circle", "Triangle"],
+      },
+    ]);
 
+    this.name = answers.name;
+    this.setText(this.name, answers.colorText);
+
+    switch (answers.shape.toLowerCase()) {
+      case "square":
+        this.setShape(new Square(answers.colorShape));
+        break;
+      case "circle":
+        this.setShape(new Circle(answers.colorShape));
+        break;
+      case "triangle":
+        this.setShape(new Triangle(answers.colorShape));
+        break;
+      default:
+        break;
     }
-    render() {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.name}${this.shape}</svg>`
-    }
-    setText(text, color) {
-        this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShape(shape){
-        this.shape = shape.render();
-    }
+  }
+
+  createFile() {
+    fs.writeFileSync("shapes.svg", this.render());
+    console.log("File shapes.svg created successfully");
+  }
 }
 
-inquire
-.prompt ([
-    {
-        type : 'input',
-        name: 'name',
-        message: 'Enter your logo name (Please note the logo name should be up to 3 characters):',
-    },
-    {
-        type:"input",
-        name: 'input',
-        message: "Enter the color of your text",
-    },
-    {
-        type:"list",
-        name: "shape",
-        message:'Select your shape style:',
-        choices: ["Square", "Circle", "Triangle"],
-    },
-])
+async function main() {
+  const svgGenerator = new Svg();
+  await svgGenerator.promptUser();
+  svgGenerator.createFile();
+}
+
+main();
